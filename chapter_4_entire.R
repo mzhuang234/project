1a5ab731fad62abd2458bb9ee66349f89275be1a
Michelle Zhuang <mzhuang234@pointloma.edu>
2024-02-16 04:59:44 +0000

    clear new commands

--- chapter_4_entire.R
+++ chapter_4_entire.R
@@ -10,4 +10,4 @@ ggplot(data=CPS85,mapping=aes(x=exper,y=wage))+geom_point()
 # delete an outlier to the exper-wage correlation
 CPS85<-CPS85[CPS85$wage<40,]
 ggplot(data=CPS85,mapping=aes(x=exper,y=wage))+geom_point()
-mean(c(3,24,365,60))
+
