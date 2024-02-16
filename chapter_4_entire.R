85acd5bc1887b10dba0d7ec45110a75c805b75ed
Michelle Zhuang <mzhuang234@pointloma.edu>
2024-02-16 03:34:18 +0000

    Add chapter_4_entire.R

---
+++ chapter_4_entire.R
@@ -0,0 +1,12 @@
+install.packages("shiny")
+install.packages(c("mosaicData","ggplot2"))
+CPS85
+ggplot(data=CPS85,mapping=aes(x=exper,y=wage))
+## Geoms are the geometric objects (points,lines,bars,and shaded regioins) that
+# can be placed on a graph. They are added using functions whose names begin
+# with geom_.
+## In ggplot2 graphs, functions are chained together using the + sign to build a final plot 
+ggplot(data=CPS85,mapping=aes(x=exper,y=wage))+geom_point()
+# delete an outlier to the exper-wage correlation
+CPS85<-CPS85[CPS85$wage<40,]
+ggplot(data=CPS85,mapping=aes(x=exper,y=wage))+geom_point()
